dd382fcfddcae8a877a31d3d802f04e94d06d3a7
ottohcharleswills <ottohcharleswills@gmail.com>
2026-09-01 19:23:51 +0100

    profile2

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -49,12 +49,12 @@ class DashboardController extends Controller
             [
                 'label' => __('Upload a profile picture'),
                 'done'  => (bool) $user->profile_photo,
-                'route' => route('profile.show'),
+                'route' => route('profile.show', ['user' => $user->id]),
             ],
             [
                 'label' => __('Upload a cover photo'),
                 'done'  => (bool) $user->cover_photo,
-                'route' => route('profile.show'),
+                'route' => route('profile.show', ['user' => $user->id]),
             ],
             [
                 'label' => __('Join a group'),
